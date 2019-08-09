6930711e8cf82092f28935535e617454a9f4f6d2
Dave Furfero <furf@furf.com>
2019-08-08 21:45:26 -0700

    Upgrade to 1.1.0 for Typescript support.

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Given a noun and a verb, chuck will return a clever poem.
+ *
+ * @param {string} noun a noun
+ * @param {string} verb a verb
+ * @return {string} a poem
+ */
+function chuck(noun, verb) {
+    return `How much ${noun} could a ${noun}${verb} ${verb} if a ${noun}${verb} could ${verb} ${noun}?`;
+}
+exports.chuck = chuck;
+//# sourceMappingURL=index.js.map
